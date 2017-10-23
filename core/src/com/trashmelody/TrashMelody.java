dfc355466fc28ad52fb019c224d31cf4dc44f36a
kavinvin <kavinvin.vin@gmail.com>
2017-10-23 17:40:53 +0700

    Add splash screen

--- core/src/com/trashmelody/TrashMelody.java
+++ core/src/com/trashmelody/TrashMelody.java
@@ -1,38 +1,27 @@
 package com.trashmelody;
 
 import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Game;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.trashmelody.screens.SplashScreen;
 
-public class TrashMelody extends ApplicationAdapter {
-	SpriteBatch batch;
-	Texture img;
+public class TrashMelody extends Game {
+	public SpriteBatch batch;
 
-	private int scrHeight;
-	private int scrWidth;
-	
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
-		img = new Texture("splash-logo.png");
+		setScreen(new SplashScreen(this));
 	}
 
 	@Override
 	public void render () {
-		scrHeight = Gdx.graphics.getHeight();
-		scrWidth = Gdx.graphics.getWidth();
 		Gdx.gl.glClearColor(1, 1, 1, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-		batch.begin();
-		batch.draw(img, (scrWidth/2)-250, (scrHeight/2)-143, 500, 286);
-		batch.end();
-	}
-	
-	@Override
-	public void dispose () {
-		batch.dispose();
-		img.dispose();
+
+		super.render();
 	}
 }
---
+++ core/src/com/trashmelody/screens/SplashScreen.java
@@ -0,0 +1,33 @@
+package com.trashmelody.screens;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.ScreenAdapter;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.Texture;
+import com.trashmelody.TrashMelody;
+
+public class SplashScreen extends ScreenAdapter {
+    private TrashMelody game;
+    public Texture img;
+    public int scrHeight;
+    public int scrWidth;
+
+    public SplashScreen(TrashMelody game) {
+        this.game = game;
+        img = new Texture("splash-logo.png");
+        scrHeight = Gdx.graphics.getHeight();
+        scrWidth = Gdx.graphics.getWidth();
+    }
+
+    @Override
+    public void render(float delta) {
+        Gdx.gl.glClearColor(1, 1, 1, 1);
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+
+        game.batch.begin();
+        game.batch.draw(img, (scrWidth/2)-250, (scrHeight/2)-143, 500, 286);
+        game.batch.end();
+    }
+
+
+}

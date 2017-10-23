5f41cf63d0423393f3803bb5250864609d9211df
Nathan Yiangsupapaanontr <zartre.y@gmail.com>
2017-10-23 17:13:33 +0700

    Change splash screen to TrashMelody

--- core/src/com/trashmelody/TrashMelody.java
+++ core/src/com/trashmelody/TrashMelody.java
@@ -9,19 +9,24 @@ import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 public class TrashMelody extends ApplicationAdapter {
 	SpriteBatch batch;
 	Texture img;
+
+	private int scrHeight;
+	private int scrWidth;
 	
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
+		img = new Texture("splash-logo.png");
 	}
 
 	@Override
 	public void render () {
-		Gdx.gl.glClearColor(1, 0, 0, 1);
+		scrHeight = Gdx.graphics.getHeight();
+		scrWidth = Gdx.graphics.getWidth();
+		Gdx.gl.glClearColor(1, 1, 1, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 		batch.begin();
-		batch.draw(img, 0, 0);
+		batch.draw(img, (scrWidth/2)-250, (scrHeight/2)-143, 500, 286);
 		batch.end();
 	}
 	

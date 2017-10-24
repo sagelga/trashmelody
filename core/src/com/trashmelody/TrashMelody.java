79a3e1c24c01f6ce0de44c5dfce123a4981087ae
kavinvin <kavinvin.vin@gmail.com>
2017-10-24 15:08:47 +0700

    Use clearScreen function in TrashMelody

--- core/src/com/trashmelody/TrashMelody.java
+++ core/src/com/trashmelody/TrashMelody.java
@@ -1,12 +1,12 @@
 package com.trashmelody;
 
 import com.badlogic.gdx.Game;
-import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.google.inject.Guice;
 import com.google.inject.Injector;
 import com.trashmelody.screens.SplashScreen;
+
+import static com.trashmelody.Utils.clearScreen;
 
 public class TrashMelody extends Game {
 	SpriteBatch batch;
@@ -14,15 +14,14 @@ public class TrashMelody extends Game {
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
-
 		Injector injector = Guice.createInjector(new GameModule(this));
+
 		setScreen(injector.getInstance(SplashScreen.class));
 	}
 
 	@Override
 	public void render () {
-		Gdx.gl.glClearColor(1, 1, 1, 1);
-		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+	    clearScreen();
 
 		super.render();
 	}

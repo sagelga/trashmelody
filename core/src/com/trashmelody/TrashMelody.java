1af75fb445fd0f285cb585cc02bf1a59fb42ca53
kavinvin <kavinvin.vin@gmail.com>
2017-10-24 11:07:00 +0700

    Use injector to inject SpriteBatch

--- core/src/com/trashmelody/TrashMelody.java
+++ core/src/com/trashmelody/TrashMelody.java
@@ -4,15 +4,19 @@ import com.badlogic.gdx.Game;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.google.inject.Guice;
+import com.google.inject.Injector;
 import com.trashmelody.screens.SplashScreen;
 
 public class TrashMelody extends Game {
-	public SpriteBatch batch;
+	SpriteBatch batch;
 
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
-		setScreen(new SplashScreen(this));
+
+		Injector injector = Guice.createInjector(new GameModule(this));
+		setScreen(injector.getInstance(SplashScreen.class));
 	}
 
 	@Override

b78d4d35c61dae99641ff26d6458914f8e348b56
kavinvin <kavinvin.vin@gmail.com>
2017-11-06 15:53:32 +0700

    Add display mode

--- desktop/src/com/trashmelody/desktop/DesktopLauncher.java
+++ desktop/src/com/trashmelody/desktop/DesktopLauncher.java
@@ -1,21 +1,22 @@
 package com.trashmelody.desktop;
 
-import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.Graphics;
+import com.badlogic.gdx.Graphics.DisplayMode;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
 import com.trashmelody.TrashMelody;
-import org.lwjgl.opengl.Display;
 
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
-		config.width = 2880;
-		config.height = 1800;
-		config.useHDPI = true;
+        DisplayMode[] primaryDesktopMode = LwjglApplicationConfiguration.getDisplayModes();
+        config.width = 2880;
+        config.height = 1800;
 
-		// Enable native fullscreen mode on macOS
-		System.setProperty("org.lwjgl.opengl.Display.enableOSXFullscreenModeAPI", "true");
+        config.setFromDisplayMode(primaryDesktopMode[2]);
+
+        for (DisplayMode mode : primaryDesktopMode) {
+            System.out.println(mode);
+        }
 
 		new LwjglApplication(new TrashMelody(), config);
 	}

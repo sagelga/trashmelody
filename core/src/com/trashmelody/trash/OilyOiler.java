47612bcfc45a3978d738c89081918004ac1ae238
Nathan Yiangsupapaanontr <zartre.y@gmail.com>
2017-11-16 21:46:59 +0700

    Add OilyOiler TRASH class

---
+++ core/src/com/trashmelody/trash/OilyOiler.java
@@ -0,0 +1,21 @@
+package com.trashmelody.trash;
+
+public class OilyOiler {
+    private String name = "Oily Oiler";
+    private String desc = "Oily oiler is an oil can from the suburb. After he had been emptied petrol, he got thrown away without care.";
+    private String type = "dangerous";
+
+    public OilyOiler() { }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getDesc() {
+        return desc;
+    }
+
+    public String getType() {
+        return type;
+    }
+}

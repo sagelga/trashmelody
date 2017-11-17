eb8511ea571141c973483281f7a77df3d394e4ff
Nathan Yiangsupapaanontr <zartre.y@gmail.com>
2017-11-17 19:29:08 +0700

    Create Dangerous package

--- core/src/com/trashmelody/trash/dangerous/ImmortalBag.java
+++ core/src/com/trashmelody/trash/dangerous/ImmortalBag.java
@@ -1,4 +1,7 @@
-package com.trashmelody.trash;
+package com.trashmelody.trash.dangerous;
+
+import com.trashmelody.trash.Trash;
+import com.trashmelody.trash.TrashType;
 
 public class ImmortalBag extends Trash {
     private static final String NAME = "Immortal bag";
--- core/src/com/trashmelody/trash/dangerous/ThinnerCarpenter.java
+++ core/src/com/trashmelody/trash/dangerous/ThinnerCarpenter.java
@@ -1,4 +1,7 @@
-package com.trashmelody.trash;
+package com.trashmelody.trash.dangerous;
+
+import com.trashmelody.trash.Trash;
+import com.trashmelody.trash.TrashType;
 
 public class ThinnerCarpenter extends Trash {
     private static final String NAME = "Thinner the Carpenter";

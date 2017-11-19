98957ba4921b21faf9f19bbf8d8a75f21de928ec
kavinvin <kavinvin.vin@gmail.com>
2017-11-19 19:42:24 +0700

    Set fullscreen from TRASH_MELODY_FULLSCREEN environment variable

--- desktop/src/com/trashmelody/desktop/DesktopLauncher.java
+++ desktop/src/com/trashmelody/desktop/DesktopLauncher.java
@@ -6,6 +6,9 @@ import com.badlogic.gdx.Files;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
 import com.trashmelody.TrashMelody;
+import com.trashmelody.utils.Environment;
+
+import static com.trashmelody.utils.Environment.getBooleanFromEnv;
 
 public class DesktopLauncher {
     private static float SCREEN_SCALE = 0.5F;
@@ -13,7 +16,7 @@ public class DesktopLauncher {
 	public static void main (String[] arg) {
 	    TrashMelody game = new TrashMelody();
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
-		config.fullscreen = false;
+		config.fullscreen = getBooleanFromEnv("TRASH_MELODY_FULLSCREEN").getOrElse(true);
 		config.width  = (int) (game.WIDTH  * SCREEN_SCALE);
 		config.height = (int) (game.HEIGHT * SCREEN_SCALE);
 		config.useHDPI = true;

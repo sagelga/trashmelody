9acce6fc5fc8f172a219f7fd0b3b83def85a2031
Nathan Yiangsupapaanontr <zartre.y@gmail.com>
2017-11-19 14:39:01 +0700

    Center trash name

--- core/src/com/trashmelody/screens/CollectionScreen.java
+++ core/src/com/trashmelody/screens/CollectionScreen.java
@@ -6,10 +6,8 @@ import com.badlogic.gdx.graphics.Camera;
 import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
-import com.badlogic.gdx.graphics.g2d.Animation;
-import com.badlogic.gdx.graphics.g2d.BitmapFont;
-import com.badlogic.gdx.graphics.g2d.SpriteBatch;
-import com.badlogic.gdx.graphics.g2d.TextureRegion;
+import com.badlogic.gdx.graphics.g2d.*;
+import com.badlogic.gdx.utils.Align;
 import com.badlogic.gdx.utils.Scaling;
 import com.badlogic.gdx.utils.viewport.ScalingViewport;
 import com.badlogic.gdx.utils.viewport.Viewport;
@@ -31,7 +29,7 @@ public class CollectionScreen extends LazyScreen {
     private OrthographicCamera camera;
     private Viewport viewport;
     private SpriteBatch batch;
-    private BitmapFont font,font2,font3,font4;
+    private BitmapFont fontTitle,font2,font3,font4;
     private float vh = getViewportHeight();
     private float vw = getViewportWidth();
     private int count = 1;
@@ -48,6 +46,7 @@ public class CollectionScreen extends LazyScreen {
     //Temporary Current Stage
     private int currentStage = 1;
     float elapsed;
+    private GlyphLayout layout = new GlyphLayout();
 
     @Inject
     CollectionScreen(TrashMelody game, OrthographicCamera camera, ScreenProvider screens, Viewport viewport,SpriteBatch batch) {
@@ -64,7 +63,7 @@ public class CollectionScreen extends LazyScreen {
         clearScreen();
 
         Texture cardToDraw;
-        String nameToDraw;
+        String nameToDraw = "";
 
         game.batch.begin();
 
@@ -89,19 +88,19 @@ public class CollectionScreen extends LazyScreen {
         switch (count) {
             case 1:
                 cardToDraw = bag;
-                font.draw(batch, "Immortal bag", vw / 2.8F, vw / 6);
+                nameToDraw = "Immortal bag";
                 font2.draw(batch, "She was born 700 years ago. And as her name says;"
                         + "\n" + "she is a plastic bag that could live through centuries.", vw / 2.9F, vw / 8);
                 break;
             case 2:
                 cardToDraw = paper;
-                font.draw(batch, "The Trio", vw / 2.35F, vw / 6);
+                nameToDraw = "The Trio";
                 font2.draw(batch, "The trio of derpy paper friends that"
                         + "\n" + "a famous artist has thrown into the bin.", vw / 2.5F, vw / 8);
                 break;
             case 3:
                 cardToDraw = popcorn;
-                font.draw(batch, "Popu-san", vw / 2.35F, vw / 6);
+                nameToDraw = "Popu-san";
                 font2.draw(batch, "He is the son of comedy director M-san.Popu-san is a popular actor." + "\n"
                                 + "   He has starred in many popular films such as Trash 1997 and" + "\n"
                                 + "        earned Oscar nominations for Get Trash 2008."
@@ -109,7 +108,7 @@ public class CollectionScreen extends LazyScreen {
                 break;
             case 4:
                 cardToDraw = spray;
-                font.draw(batch, "Hairspray chan", vw / 2.95F, vw / 6);
+                nameToDraw = "Hairspray chan";
                 font2.draw(batch, "Fired from a beauty salon being accused of causing global warming, " + "\n"
                                 + "       she then determined to founding her own salon " + "\n"
                                 + "       with Wax-kung and Gel-kung to take revenge."
@@ -117,14 +116,14 @@ public class CollectionScreen extends LazyScreen {
                 break;
             case 5:
                 cardToDraw = note;
-                font.draw(batch, "Pep", vw / 2.05F, vw / 6);
+                nameToDraw = "Pep";
                 font2.draw(batch, "   The lost piece of Pep Guardiola’s note, " + "\n"
                                 + "so the name \"Pep\" literally comes from his owner."
                         , vw / 2.7F, vw / 8);
                 break;
             case 6:
                 cardToDraw = donut;
-                font.draw(batch, "Dono-chan", vw / 2.45F, vw / 6);
+                nameToDraw = "Dono-chan";
                 font2.draw(batch, "  Dono-Chan is a teacher of Circle Dance and Sing Academy." + "\n"
                                 + "       Although she is fat. she can dance very well. " + "\n"
                                 + "         She and everyone is jealous of her talent."
@@ -132,7 +131,7 @@ public class CollectionScreen extends LazyScreen {
                 break;
             case 7:
                 cardToDraw = cigar;
-                font.draw(batch, "Cigar", vw / 2.2F, vw / 6);
+                nameToDraw = "Cigar";
                 font2.draw(batch, "A friend of every man *Cough*. But his health *Cough* " + "\n" +
                                 "    is not very well lately *Cough* due to *Cough* " + "\n" +
                                 "  his oral cavity, larynx, esophagus, and lung cancer."
@@ -140,14 +139,14 @@ public class CollectionScreen extends LazyScreen {
                 break;
             case 8:
                 cardToDraw = plastic;
-                font.draw(batch, "SaiSai", vw / 2.2F, vw / 6);
+                nameToDraw = "SaiSai";
                 font2.draw(batch, "A plastic box that wants to find new friends " + "\n" +
                                 "          and go explore the world."
                         , vw / 2.7F, vw / 8);
                 break;
             case 9:
                 cardToDraw = curry;
-                font.draw(batch, "Keri-a", vw / 2.2F, vw / 6);
+                nameToDraw = "Keri-a";
                 font2.draw(batch, "     Keri-a is the hottest girl in Trash World. " + "\n" +
                                 "  She had her red lips cosmetically enhanced. " + "\n" +
                                 "She is the Miss Popular Vote in the Trash World."
@@ -155,21 +154,21 @@ public class CollectionScreen extends LazyScreen {
                 break;
             case 10:
                 cardToDraw = thinner;
-                font.draw(batch, "Thinner the Carpenter", vw / 4.05F, vw / 6);
+                nameToDraw = "Thinner the Carpenter";
                 font2.draw(batch, "A hot and flammable guy. His smell can cause pleasant " + "\n" +
                                 "           hallucinations to everyone near him."
                         , vw / 3.1F, vw / 8);
                 break;
             case 11:
                 cardToDraw = glass;
-                font.draw(batch, "MookMook", vw / 2.5F, vw / 6);
+                nameToDraw = "MookMook";
                 font2.draw(batch, "An empty plastic glass from a bubble milk tea shop. " + "\n" +
                                 "         He is finding a way back to the shop."
                         , vw / 2.9F, vw / 8);
                 break;
             case 12:
                 cardToDraw = matcha;
-                font.draw(batch, "Matty", vw / 2.3F, vw / 6);
+                nameToDraw = "Matty";
                 font2.draw(batch, "   His full name is Matcha-sama. He has dark-green eyes, " + "\n" +
                                 "and he has a lot of success with ladies. He has been voted " + "\n" +
                                 "        the sexiest man in Trash World many times."
@@ -177,21 +176,21 @@ public class CollectionScreen extends LazyScreen {
                 break;
             case 13:
                 cardToDraw = can;
-                font.draw(batch, "Oily Oiler", vw / 2.5F, vw / 6);
+                nameToDraw = "Oily Oiler";
                 font2.draw(batch, "                Oily Oiler is an oil can from the suburb. " + "\n" +
                                 "After he had been emptied petrol, he got thrown away without care."
                         , vw / 3.45F, vw / 8);
                 break;
             case 14:
                 cardToDraw = cardboard;
-                font.draw(batch, "Bokk Kung", vw / 2.5F, vw / 6);
+                nameToDraw = "Bokk Kung";
                 font2.draw(batch, "          A cardboard box that used to contain a dog. " + "\n" +
                                 "He hopes to find a new dog and he’d bark \"Box-Box\" like a dog."
                         , vw / 3.3F, vw / 8);
                 break;
             case 15:
                 cardToDraw = icecream;
-                font.draw(batch, "Izu-chan", vw / 2.3F, vw / 6);
+                nameToDraw = "Izu-chan";
                 font2.draw(batch, "    Her full name is Izu - Pink Cremu. She is a sweetened frozen girl " + "\n" +
                                 "you'll want to eat if you see one. Her cheek is pink and her hair is white."
                         , vw / 3.45F, vw / 8);
@@ -201,6 +200,8 @@ public class CollectionScreen extends LazyScreen {
         }
 
         game.batch.draw(cardToDraw, vw / 2.52F, vh / 3.1F, vw / 5, vh / 2.2F);
+        layout.setText(fontTitle, nameToDraw, Color.WHITE, vw, Align.center, true);
+        fontTitle.draw(batch, layout, 0, vw / 6);
 
         if (count == 1 || count == 4 || count == 7 || count == 10 || count == 13) {
             game.batch.draw(dangerBin, vw / 1.75F, vh / 3.2F, vw / 19.5F, vh / 14);
@@ -321,7 +322,7 @@ public class CollectionScreen extends LazyScreen {
         this.wetBin     = assets.get(GAME_BIN_03, TEXTURE);
         this.idkBin     = assets.get(GAME_BIN_04, TEXTURE);
 
-        this.font       = assets.get8bitFont(40, Color.WHITE);
+        this.fontTitle       = assets.get8bitFont(40, Color.WHITE);
         this.font2      = assets.getSuperSpaceFont(24, Color.WHITE);
         this.font3       = assets.get8bitFont(24, Color.WHITE);
         this.font4       = assets.getSuperSpaceFont(24, Color.WHITE);
